bff79ffc0028a39edd1396a59122171c2d99cbce
Henrique Leao <henrique.s3.leao@gmail.com>
2021-01-28 06:28:30 -0300

    refactor: change in logic error status

--- script.js
+++ script.js
@@ -6,16 +6,18 @@ let highScore = 0;
 const secretNumber = Math.trunc(Math.random()*20) + 1;
 document.querySelector('.score').textContent = score;
 
-document.querySelector('.check').addEventListener('click', handleValueInput);
+document.querySelector('.check').addEventListener('click', compareInputAndSecretNumber);
 document.querySelector('body').addEventListener('keydown', handleClickEnter);
-document.querySelector('.again').addEventListener('click', handleReloadPage);
+document.querySelector('.again').addEventListener('click', handleResetStatesPage);
 
-function handleValueInput() {
-  console.log(secretNumber)
+function handleInputNumber() {
   const guess = Number(document.querySelector('.guess').value);
   if(!guess) document.querySelector('.message').textContent = '⛔️ No number';
-  
-  if(secretNumber === guess) {
+  handleCompareInput(guess);
+};
+
+function handleCompareInput(number) {
+  if(secretNumber === number) {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.message').textContent = '🍕 Correct Number';
     handleChangeColor('#3dd30c');
@@ -25,16 +27,16 @@ function handleValueInput() {
     }
   }
 
-  if(guess > secretNumber) {
+  if(number > secretNumber) {
     document.querySelector('.message').textContent = '😙 too high!';
     decreasesScore();
   }
 
-  if(guess < secretNumber && guess > 0) {
+  if(number < secretNumber && number > 0) {
     document.querySelector('.message').textContent = '😙 too low!';
     decreasesScore();
   }
-};
+}
 
 function handleClickEnter(event) {
   if(event.key === 'Enter') {
@@ -46,8 +48,8 @@ function decreasesScore () {
   if(score <= 1) {
     document.querySelector('.message').textContent = '☠️ you lost the game!';
     document.querySelector('.check').setAttribute('disabled', true);
-    score--;
-    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.score').textContent = 0;
     handleChangeColor('red');
     return;
   };
@@ -55,9 +57,10 @@ function decreasesScore () {
   document.querySelector('.score').textContent = score;
 }
 
-function handleReloadPage() {
+function handleResetStatesPage() {
   const color = '#222';
   score = chances;
+  secretNumber = Math.trunc(Math.random()*20) + 1;
   document.querySelector('main').style.backgroundColor = color;
   document.querySelector('header').style.backgroundColor = color;
   document.querySelector('.score').textContent = score;
@@ -71,3 +74,4 @@ function handleChangeColor(color) {
   document.querySelector('main').style.backgroundColor = color;
   document.querySelector('header').style.backgroundColor = color;
 }
+
